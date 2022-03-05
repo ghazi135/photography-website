eee89ede3f5d81357f65673122f7f8a67dc4a489
Ghazi BOUZAZI <gbouzazi@gmail.com>
2022-03-05 13:30:54 +0100

    UPDATE PROJECT

--- src/component/project/Project.tsx
+++ src/component/project/Project.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './project.css';
 // import data from '../../data/projectData.json'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Button, Card, CardActions, CardContent, Fade, Typography} from "@mui/material";
+import { Fade, Grid} from "@mui/material";
 
 function Project() {
     // const DisplayData=data.map(
@@ -18,33 +18,50 @@ function Project() {
         <Fade in timeout={{ enter: 1200 }}>
             <div className="Project">
                 {/*{DisplayData}*/}
-            <Card sx={{
-                mx: 'auto',
-                color: 'white',
-                backgroundColor: '#2a2b2e',
-                boxShadow: '10px 5px 5px black',
-                width:'30rem',
-                height: '30rem',
-                border: 1,
-                fontSize: 14,
-                borderColor: 'coral',
-                justifyContent: 'center',
-                borderRadius: 2,
-                overflow: 'auto'
-            }}>
-                <CardContent>
-                    <Typography >
+
+                <Grid container sx={{
+                    mx: 'auto',
+                    color: 'white',
+                    backgroundColor: 'none',
+                    boxShadow: '10px 5px 5px black',
+                    border: 1,
+                    maxWidth: '60%',
+                    fontSize: 14,
+                    padding: '20px',
+                    borderColor: 'white',
+                    justifyContent: 'center',
+                    borderRadius: 2,
+                    overflow: 'auto'
+                }}>
+                    <Grid item xs>
                         Word of the Day
                         adjective
-                        <span className="divider"/>
                         Word of the Day
                         adjective
-                    </Typography>
-                </CardContent>
-                <CardActions>
-                    <Button size="small">Code</Button>
-                </CardActions>
-            </Card>
+                        Word of the Day
+                        adjective
+                        Word of the Day
+                        adjective
+                        Word of the Day
+                        adjective Word of the Day
+                        adjective
+                    </Grid>
+                    <div className="sep-text">
+                        '
+                    </div>
+                    <Grid item xs>
+                        Word of the Day
+                        adjective
+                        Word of the Day
+                        adjective
+                        Word of the Day
+                        adjective
+                        Word of the Day
+                        adjective
+                        Word of the Day
+                        adjective Word of the Day
+                        adjective                    </Grid>
+                </Grid>
             </div>
         </Fade>
 
