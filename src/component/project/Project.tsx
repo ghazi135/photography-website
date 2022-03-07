0d6c5bc192f6d973517d28ca133e6cd6e894c4a1
Ghazi BOUZAZI <gbouzazi@gmail.com>
2022-03-07 02:37:10 +0100

    UPDATE PROJECT

--- src/component/project/Project.tsx
+++ src/component/project/Project.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './project.css';
 // import data from '../../data/projectData.json'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Fade, Grid} from "@mui/material";
+import {Fade, Grid, Hidden} from "@mui/material";
 import picture from "../../assets/safetyNet.png";
 
 function Project() {
@@ -16,42 +16,86 @@ function Project() {
     //     }
     // )
     return (
-        <Fade in timeout={{ enter: 1200 }}>
+        <Fade in timeout={{enter: 1200}}>
             <div className="Project">
                 {/*{DisplayData}*/}
-
-                <Grid container sx={{
-                    mx: 'auto',
-                    color: 'white',
-                    backgroundColor: 'none',
-                    boxShadow: '10px 5px 5px black',
-                    border: 1,
-                    maxWidth: '60%',
-                    fontSize: 14,
-                    justifyContent: 'center',
-                    padding: '10px',
-                    borderColor: 'white',
-                    borderRadius: 2,
-                    overflow: 'auto'
-                }}>
+                <Hidden mdDown>
+                    <Grid container sx={{
+                        mx: 'auto',
+                        color: 'white',
+                        backgroundColor: 'none',
+                        boxShadow: '10px 5px 5px black',
+                        border: 1,
+                        maxWidth: '60%',
+                        fontSize: 14,
+                        justifyContent: 'center',
+                        padding: '8px',
+                        borderColor: 'white',
+                        borderRadius: 2,
+                        overflow: 'auto'
+                    }}>
 
                         <img className="img-project" src={picture} alt="ghazi"/>
 
-                    <div className="line"></div>
+                        <div className="line"></div>
 
-                    <p>
-                        The purpose of this application is to send information to emergency service systems. Your role will be to create the back-end of this new application from scratch.
-                        To give you more clarification on usage, here are some examples:
-                        If, for example, a fire breaks out, SafetyNet Alerts needs to provide information about the people in the burning building, such as their phone numbers.
-                        In the event of a hurricane alert, we want SafetyNet Alerts to be able to text everyone in the area. To do this, SafetyNet Alerts needs to collect the phone numbers of people living in homes near the hurricane area.
-                        In the event of a flood, we want to provide emergency services with specific information about people in the area. We need to know the potential victims, their ages and their medical history (treatments, allergies, etc.).
-                    </p>
+                        <p>
+                            The purpose of this application is to send information to emergency service systems. Your
+                            role will be to create the back-end of this new application from scratch.
+                            To give you more clarification on usage, here are some examples:
+                            If, for example, a fire breaks out, SafetyNet Alerts needs to provide information about the
+                            people in the burning building, such as their phone numbers.
+                            In the event of a hurricane alert, we want SafetyNet Alerts to be able to text everyone in
+                            the area. To do this, SafetyNet Alerts needs to collect the phone numbers of people living
+                            in homes near the hurricane area.
+                            In the event of a flood, we want to provide emergency services with specific information
+                            about people in the area. We need to know the potential victims, their ages and their
+                            medical history (treatments, allergies, etc.).
+                        </p>
 
-                </Grid>
+                    </Grid>
+                </Hidden>
+
+                <Hidden mdUp>
+                    <Grid container sx={{
+                        mx: 'auto',
+                        color: 'white',
+                        backgroundColor: 'none',
+                        boxShadow: '10px 5px 5px black',
+                        border: 1,
+                        maxWidth: '90%',
+                        fontSize: 14,
+                        justifyContent: 'center',
+                        padding: '8px',
+                        borderColor: 'white',
+                        borderRadius: 2,
+                        overflow: 'auto'
+                    }}>
+
+                        <img className="img-project" src={picture} alt="ghazi"/>
+
+                        <div className="line"></div>
+
+                        <p>
+                            The purpose of this application is to send information to emergency service systems. Your
+                            role will be to create the back-end of this new application from scratch.
+                            To give you more clarification on usage, here are some examples:
+                            If, for example, a fire breaks out, SafetyNet Alerts needs to provide information about the
+                            people in the burning building, such as their phone numbers.
+                            In the event of a hurricane alert, we want SafetyNet Alerts to be able to text everyone in
+                            the area. To do this, SafetyNet Alerts needs to collect the phone numbers of people living
+                            in homes near the hurricane area.
+                            In the event of a flood, we want to provide emergency services with specific information
+                            about people in the area. We need to know the potential victims, their ages and their
+                            medical history (treatments, allergies, etc.).
+                        </p>
+
+                    </Grid>
+                </Hidden>
             </div>
         </Fade>
 
-);
+    );
 }
 
 export default Project;
