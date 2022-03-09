38a625fb1e21a2d75d95f66d62219f91a1ab5acf
Ghazi BOUZAZI <gbouzazi@gmail.com>
2022-03-09 02:27:04 +0100

    COMPLETE PROJECT

--- src/component/project/Project.tsx
+++ src/component/project/Project.tsx
@@ -31,7 +31,7 @@ const ProjectCard = (props: { image: string; width: any; height: any; title: obj
                             stack :  <span className='ProjectsCardMobile-grid-right'> {props.stack} </span>
                         </div>
 
-                        <a className="code" target='_blank'  href={props.buttons.link}><span>{props.buttons.name}</span></a>
+                        <a target="_blank" rel="noreferrer noopener" className="code" href={props.buttons.link}><span>{props.buttons.name}</span></a>
                         <div className="line"></div>
                         <div><h1>{props.title}</h1></div>
                         {props.description}
