614071a24ef15a9c850f915f63db5cd3a4e5a184
Ghazi BOUZAZI <gbouzazi@gmail.com>
2022-03-16 22:12:13 +0100

    organizes images

--- src/component/portrait/PortraitPhotos.tsx
+++ src/component/portrait/PortraitPhotos.tsx
@@ -1,19 +1,17 @@
-import photo1 from '../../constants/photography/_MG_3773.jpg';
-import photo2 from '../../constants/photography/_MG_3778.jpg';
-import photo3 from '../../constants/photography/_MG_3782.jpg';
-import photo4 from '../../constants/photography/IMG_2649.jpg';
-import photo5 from '../../constants/photography/IMG_2691.jpg';
-import photo6 from '../../constants/photography/IMG_2971.jpg';
-import photo7 from '../../constants/photography/IMG_2975.jpg';
-import photo8 from '../../constants/photography/IMG_3359.jpg';
-import photo9 from '../../constants/photography/IMG_2989.jpg';
-import photo10 from '../../constants/photography/IMG_3389.jpg';
-import photo11 from '../../constants/photography/IMG_3362.jpg';
-import photo12 from '../../constants/photography/IMG_3354_1.jpg';
-import photo13 from '../../constants/photography/IMG_3334.jpg';
-import photo14 from '../../constants/photography/IMG_3298.jpg';
-import photo15 from '../../constants/photography/IMG_3295.jpg';
-import photo16 from '../../constants/photography/IMG_3266.jpg';
+import photo1 from '../../constants/portrait/_MG_7578.jpg';
+
+import photo2 from '../../constants/portrait/_MG_7580.jpg';
+
+import photo3 from '../../constants/portrait/_MG_7579.jpg';
+import photo4 from '../../constants/portrait/_MG_8734.jpg';
+import photo5 from '../../constants/portrait/_MG_8669.jpg';
+import photo6 from '../../constants/portrait/_MG_8673.jpg';
+
+import photo7 from '../../constants/portrait/_MG_8749.jpg';
+import photo8 from '../../constants/portrait/_MG_7833.jpg';
+import photo9 from '../../constants/portrait/_MG_7899.jpg';
+
+import photo10 from '../../constants/portrait/_MG_7908.jpg';
 
 
 
@@ -22,89 +20,57 @@ import photo16 from '../../constants/photography/IMG_3266.jpg';
 export const portraitPhotos = [
     {
         src: photo1,
-        width: 10,
+        width: 12,
         height: 8
     },
     {
         src: photo2,
-        width: 10,
+        width: 12,
         height: 8
     },
     {
         src: photo3,
-        width: 10,
+        width: 12,
         height: 8
     },
     {
         src: photo4,
         width: 10,
-        height: 8
+        height: 16
     },
     {
         src: photo5,
-        width: 8,
+        width: 11,
         height: 8
     }
     ,
     {
         src: photo6,
-        width: 5,
-        height: 8
+        width: 6,
+        height: 9
     }
     ,
     {
         src: photo7,
-        width: 10,
-        height: 8
+        width: 6,
+        height: 9
     },
 
     {
         src: photo8,
-        width: 10,
+        width: 9,
         height: 13
     },
 
     {
         src: photo9,
-        width: 13,
-        height: 8
+        width: 8,
+        height: 11
     },
     {
         src: photo10,
-        width: 10,
-        height: 8
-    },
-    {
-        src: photo11,
         width: 6,
-        height: 8
-    },
-
-    {
-        src: photo12,
-        width: 10,
-        height: 12
-    },
-    {
-        src: photo13,
-        width: 15,
-        height: 10
-    },
-    {
-        src: photo14,
-        width: 6,
-        height: 8
-    },
-    {
-        src: photo15,
-        width: 20,
-        height: 15
-    },
-    {
-        src: photo16,
-        width: 13,
         height: 8
     }
-
 ];
 
